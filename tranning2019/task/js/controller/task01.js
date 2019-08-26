09543365e42fae06340110c93a3241e53a2d88cc
= <=>
2019-08-27 01:42:16 +0700

    ok

--- tranning2019/task/js/controller/task01.js
+++ tranning2019/task/js/controller/task01.js
@@ -4,4 +4,17 @@ angular.module('task01', []).controller('task01Controller',['$scope', '$http', '
     $scope.showListUser = response.data;
     console.log( response);
     });
+    function sumDigit(number){
+      var sum = 0;
+      var digit = 0;
+      while(number > 0){
+        digit = (number%10);
+        sum += digit;
+        number = (number - digit)/10;
+      }
+      return sum;
+    }
+    var rs = sumDigit(2324);
+    var demo = 235%10;
+    console.log(demo);
   }])
