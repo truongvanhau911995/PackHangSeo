c1bdca092c4a66986f851c5f88e3d504b1eac2ed
= <=>
2019-11-13 06:57:18 +0700

    ok

--- task01/js/date.js
+++ task01/js/date.js
@@ -18,3 +18,71 @@ dates.forEach(element => {
            console.log('dates in range', date);
         }
 })
+
+public class Student {
+   String rollNo;
+   String fullname;
+
+   public Student (String rollNo, String fullname){
+      this.rollNo = rollNo;
+      this.fullname = fullname;
+   }
+}
+
+HashMap<String, Student> studentList = new HashMap<>();
+Student std = new Student("MS01", "Truong van hau");
+studentList.pust(std.rollNo,std);
+
+Student std = new Student("MS02", "Truong van phuong");
+studentList.pust(std.rollNo,std);
+
+Student std2 = studentList.get("MS01");
+// Duyet xem co bao nhieu key trong HashMap
+Set<String> keys = map.keySet();
+keys.forEach((key) -> {
+   System.out.println("key : " + key);
+});
+
+import java.util.ArrayList;
+
+public class Controller<E>{
+   ArrayList<E> list = new ArrayList<>();
+   publish Controller(){
+   }
+   publish void addEventListener(E e){
+      list.add(e);
+   }
+   publish E get(int index){
+      return list.get(index);
+   }
+}
+
+Controller<String> controller = new Controller<>();
+controller.add("A");
+
+
+publish class Peple {
+   publish String fullname;
+}
+
+public class Student extends Peple{
+   String rollNo;
+   //String fullname;
+
+   public Student (String rollNo, String fullname){
+      this.rollNo = rollNo;
+      this.fullname = fullname;
+   }
+}
+
+public class Controller<E extends People>{
+   ArrayList<E> list = new ArrayList<>();
+   publish Controller(){
+   }
+   publish void addEventListener(E e){
+      list.add(e);
+   }
+   publish E get(int index){
+      return list.get(index);
+   }
+}
